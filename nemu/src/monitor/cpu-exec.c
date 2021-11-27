3ffceaef8df4026507679dbfad89121b0c3f5f3b
liaozetao <1107136310@qq.com>
2021-11-27 15:30:29 +0800

    添加mac兼容

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -35,10 +35,17 @@ void rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret) {
 }
 
 void monitor_statistic() {
+  #ifdef __APPLE__
+  Log("total guest instructions = %llu", g_nr_guest_instr);
+  Log("host time spent = %llu ms", g_timer);
+  if (g_timer > 0) Log("simulation frequency = %llu instr/s", g_nr_guest_instr * 1000 / g_timer);
+  else Log("Finish running in less than 1 ms and can not calculate the simulation frequency");
+  #else
   Log("total guest instructions = %ld", g_nr_guest_instr);
   Log("host time spent = %ld ms", g_timer);
   if (g_timer > 0) Log("simulation frequency = %ld instr/s", g_nr_guest_instr * 1000 / g_timer);
   else Log("Finish running in less than 1 ms and can not calculate the simulation frequency");
+  #endif
 }
 
 bool log_enable() {
