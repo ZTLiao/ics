7d78dd776cd1eadf6f1cecaa427b7bb2b084f5dd
liaozetao <1107136310@qq.com>
2023-03-01 13:17:17 +0800

    clean

--- am-kernels/tests/am-tests/build/x86-nemu/src/tests/audio/audio-data.d
+++
@@ -1,2 +0,0 @@
-/home/liaozetao/Documents/ics/am-kernels/tests/am-tests/build/x86-nemu/src/tests/audio/audio-data.o: \
- /home/liaozetao/Documents/ics/am-kernels/tests/am-tests/src/tests/audio/audio-data.S
--- am-kernels/tests/am-tests/build/x86-nemu/src/tests/rtc.d
+++
@@ -1,8 +0,0 @@
-/home/liaozetao/Documents/ics/am-kernels/tests/am-tests/build/x86-nemu/src/tests/rtc.o: \
- /home/liaozetao/Documents/ics/am-kernels/tests/am-tests/src/tests/rtc.c \
- /home/liaozetao/Documents/ics/am-kernels/tests/am-tests/include/amtest.h \
- /home/liaozetao/Documents/ics/abstract-machine/am/include/am.h \
- /home/liaozetao/Documents/ics/abstract-machine/am/include/arch/x86-nemu.h \
- /home/liaozetao/Documents/ics/abstract-machine/am/include/amdev.h \
- /home/liaozetao/Documents/ics/abstract-machine/klib/include/klib.h \
- /home/liaozetao/Documents/ics/abstract-machine/klib/include/klib-macros.h
